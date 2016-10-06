b19cd721d12a9cdc0d3748e43142d68caac11e79
pianoguys <sumanth95@yahoomail.co.in>
2016-10-06 11:40:17 -0600

    modified HelloWorld to gethub

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@
 
 class HelloWorld{
      public static void main(String args[]){
-     	// prints the line Hello World
-          System.out.println("Hello World\n");
+     	// prints the line Hello GitHub World
+          System.out.println("Hello GitHub World\n");
       }
 }
